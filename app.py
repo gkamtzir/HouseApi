56399781296211eb3a7089ea9d09904e50b95fc7
George Kamtziridis <georgekam96@gmail.com>
2018-12-26 11:51:44 +0200

    Added door frame types

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask_restful import Api
 from resources.User import UserResource
 from resources.PropertySupervisor import PropertySupervisorResource
 from resources.City import CityResource
+from resources.DoorFrameType import DoorFrameTypeResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Routes
-api.add_resource(UserResource, '/User')
-api.add_resource(PropertySupervisorResource, '/PropertySupervisor')
-api.add_resource(CityResource, '/City')
+api.add_resource(UserResource, "/User")
+api.add_resource(PropertySupervisorResource, "/PropertySupervisor")
+api.add_resource(CityResource, "/City")
+api.add_resource(DoorFrameTypeResource, "/DoorFrameType")
---
+++ resources/DoorFrameType.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+from models.DoorFrameType import DoorFrameType,\
+    DoorFrameTypeSchema
+
+door_frame_types_schema = DoorFrameTypeSchema(many=True)
+door_frame_type_schema = DoorFrameTypeSchema()
+
+
+class DoorFrameTypeResource(Resource):
+    def get(self):
+        door_frame_types = DoorFrameType.query.all()
+        door_frame_types = door_frame_types_schema\
+            .dump(door_frame_types).data
+        return {"status": "success", "data": door_frame_types}, 200
