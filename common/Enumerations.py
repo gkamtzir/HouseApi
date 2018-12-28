fa83516d25e5d1c6508507b711bd921b7104a3a0
George Kamtziridis <georgekam96@gmail.com>
2018-12-28 16:50:57 +0200

    Changed the way enumerations are defined

--- common/Enumerations.py
+++ common/Enumerations.py
@@ -1,30 +1,46 @@
 import enum
 
 
-class GlassTypeEnum(enum.Enum):
-    Single = 1
-    Double = 2
-    Triple = 3
-    Other = 4
+GlassTypeEnum = enum.Enum(
+    value="GlassTypeEnum",
+    names=[
+        ("Single", 1),
+        ("Double", 2),
+        ("Triple", 3),
+        ("Other", 4)
+    ]
+)
 
 
-class HeatingTypeEnum(enum.Enum):
-    Decentralized = 1
-    Central = 2
-    Other = 3
+HeatingTypeEnum = enum.Enum(
+    value="HeatingTypeEnum",
+    names=[
+        ("Decentralized", 1),
+        ("Central", 2),
+        ("Other", 3)
+    ]
+)
 
 
-class PropertyActionEnum(enum.Enum):
-    Sale = 1
-    Rent = 2
+PropertyActionEnum = enum.Enum(
+    value="PropertyActionEnum",
+    names=[
+        ("Sale", 1),
+        ("Rent", 2)
+    ]
+)
 
 
-class VisitStatusEnum(enum.Enum):
-    PendingVerification = 1
-    Scheduled = 2
-    Canceled = 3
-    Completed = 4
-    Failed = 5
+VisitStatusEnum = enum.Enum(
+    value="VisitStatusEnum",
+    names=[
+        ("Pending Verification", 1),
+        ("Scheduled", 2),
+        ("Canceled", 3),
+        ("Completed", 4),
+        ("Failed", 5),
+    ]
+)
 
 
 PropertyTypeEnum = enum.Enum(
