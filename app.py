ab95d0a2bbbcda1f7dca3b9eb9e38fc2b0645d3d
George Kamtziridis <georgekam96@gmail.com>
2019-01-12 11:22:20 +0200

    Added supervisor login

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from resources.Login import LoginResource
 from resources.UserFavorites import UserFavoritesResource
 from resources.Search import SearchResource
 from resources.AddFavorite import AddFavoriteResource
+from resources.LoginSupervisor import LoginSupervisorResource
 
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
@@ -40,3 +41,4 @@ api.add_resource(LoginResource, "/Login")
 api.add_resource(UserFavoritesResource, "/UserFavorite/")
 api.add_resource(SearchResource, "/Search")
 api.add_resource(AddFavoriteResource, "/AddFavorite/<int:property_id>")
+api.add_resource(LoginSupervisorResource, "/LoginSupervisor")
---
+++ resources/LoginSupervisor.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource, request
+from models.PropertySupervisor import PropertySupervisor, \
+    PropertySupervisorSchema
+from common.Authentication import encode_auth_token
+
+property_supervisor_schema = PropertySupervisorSchema()
+
+
+class LoginSupervisorResource(Resource):
+    def post(self):
+        post_data = request.get_json()
+        try:
+            supervisor = PropertySupervisor.query.\
+                filter_by(contact_email=post_data.get("email")).first()
+            if supervisor is None:
+                return {"status": "Fail", "message": ("Supervisor does not"
+                                                      " exist.")}, 404
+            else:
+                supervisor = property_supervisor_schema.dump(supervisor).data
+                # Generate token.
+                token = str(encode_auth_token(supervisor["id"], "supervisor"))
+                # Removed token quotes.
+                token = token[2:len(token) - 1]
+                return {"status": "Success",
+                        "token": token}, 200
+        except Exception as e:
+            print(e)
+            return {"status": "Fail", "message": "Try again"}, 500
